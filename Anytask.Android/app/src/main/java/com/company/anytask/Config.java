0df881256e16b55e4dbdf05bd7ba5c9917ee40a1
fakefeik <feikfake1@gmail.com>
2016-06-16 13:21:54 +0500

    fix mock api url

--- Anytask.Android/app/src/main/java/com/company/anytask/Config.java
+++ Anytask.Android/app/src/main/java/com/company/anytask/Config.java
@@ -1,14 +1,5 @@
 package com.company.anytask;
-import android.annotation.TargetApi;
-import android.os.Build;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Date;
-import java.util.Properties;
-import java.util.StringTokenizer;
 
 public class Config {
-      public static final String API_URL = "http://localhost:43818/api/";
+    public static final String API_URL = "http://mock-anytask.azurewebsites.net/api/";
 }
