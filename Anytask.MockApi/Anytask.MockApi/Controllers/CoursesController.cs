603690b85140902644eaf80f90be28e1e5e9127c
fakefeik <feikfake1@gmail.com>
2016-06-28 11:32:42 +0500

    add CourseTasks

--- Anytask.MockApi/Anytask.MockApi/Controllers/CoursesController.cs
+++ Anytask.MockApi/Anytask.MockApi/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
@@ -7,6 +8,7 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Anytask.MockApi.Database;
 using Domain.Anytask;
+using Task = Domain.Anytask.Task;
 
 namespace Anytask.MockApi.Controllers
 {
@@ -57,6 +59,48 @@ namespace Anytask.MockApi.Controllers
         {
             return db.Courses
                 .Include(c => c.Organization);
+        }
+
+        [Route("Courses/{id}/CourseTasks")]
+        public CourseTasks GetCourseTasks(int id)
+        {
+            var ct = new CourseTasks();
+            var course = db.Courses
+                .Include(c => c.Organization)
+                .FirstOrDefault(c => c.Id == id);
+            if (course == null)
+                return null;
+            var students = course.Students.Select(s => new User { Id = s.Id, Name = s.UserName }).ToList();
+            var tasks = course.Tasks;
+            ct.Students = students;
+            ct.Tasks = tasks;
+            var studentTasks = new Dictionary<string, Dictionary<int, Score>>();
+            var scores = db.Scores.ToList();
+            foreach (var student in students)
+            {
+                studentTasks[student.Id] = new Dictionary<int, Score>();
+                foreach (var task in tasks)
+                {
+                    var score = scores
+                        .FirstOrDefault(s => s.Task.Id == task.Id && s.Student.Id == student.Id);
+                    if (score == null)
+                    {
+                        studentTasks[student.Id][task.Id] = null;
+                        continue;
+                    }
+                    studentTasks[student.Id][task.Id] = new Score
+                    {
+                        Id = score.Id,
+                        Status = score.Status,
+                        Comments = score.Comments,
+                        Student = null,
+                        Task = null,
+                        Value = score.Value
+                    };
+                }
+            }
+            ct.UserTasks = studentTasks;
+            return ct;
         }
 
         // GET: api/Courses/5
---
+++ Anytask.MockApi/Domain/Anytask/CourseTasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Anytask
+{
+    public class CourseTasks
+    {
+        public ICollection<User> Students;
+        public ICollection<Task> Tasks;
+        public Dictionary<string, Dictionary<int, Score>> UserTasks;
+    }
+}
